fe7de3a8a99645168344307b98c76e0e9266aa73
Bat_Gosho <goshkodark25@gmail.com>
2024-11-30 16:46:16 +0200

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace WhriteTextToFile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string message = "hello";
+            int count = 0;
+
+            while (true)
+            {
+                Console.WriteLine(message);
+                count++;
+                if(count > 9)
+                {
+                    break;
+                }
+            }
+            */
+            for (int i = 0; i < 100; i += 3)
+            {
+                // Don't change above this line
+
+                if (i > 21)
+                {
+                    break;
+                }
+    
+            // Don't change below this line
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
