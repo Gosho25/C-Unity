083cd5f9067f91a0af4e6982c37d373a78f8fe65
Bat_Gosho <goshkodark25@gmail.com>
2024-12-02 16:11:11 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Project2
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Enter your age:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int toStoIDvaiset = 120 - userInput;
+            Console.WriteLine(toStoIDvaiset);
+            */
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            for(int i = 0; i <= num*3; i++)
+            {
+                i += num;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
