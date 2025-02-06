9476fbf0a73ca4bc080fa4f9f3a865626dfca10f
Bat_Gosho <goshkodark25@gmail.com>
2025-02-06 22:27:21 +0200

    Create PracticeWithTheBook.cs

---
+++ PracticeWithTheBook.cs
@@ -0,0 +1,33 @@
+namespace PracticeWithTheBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toFive = int.Parse(Console.ReadLine());
+            switch(toFive)
+            {
+                case 1:
+                    Console.WriteLine("Your number is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Your number is 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Your number is 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Your number is 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Your number is 5");
+                    break;
+                default:
+                    Console.WriteLine("Your number is 0 or something else");
+                    break;
+
+            }
+            Math.Pow(2, toFive);
+        }
+    }
+}
