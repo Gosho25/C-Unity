c3c3aa2983bb8bd140a5e198d062d973fc2c866a
Bat_Gosho <goshkodark25@gmail.com>
2025-02-12 19:52:59 +0200

    Create FirstFile.cs

---
+++ FirstFile.cs
@@ -0,0 +1,20 @@
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.InputSystem.XR;
+using static UnityEngine.GraphicsBuffer;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstFile : MonoBehaviour
+{
+    [SerializeField] private float speed = 8f;//скорост
+    [SerializeField] private float rotationSpeed = 70;//скорост на въртене
+    [SerializeField] private float gravity = -26f;//сила на гравитацията(отрилцателна, за да дърпа към земята)
+    [SerializeField] private float jumpSpeed = 10;//скорост на скок   
+    [SerializeField] private float airControlSpeed = 7f;
+    [SerializeField] private float sprinting = 15f;//тичане 
+
+    [SerializeField] private Transform camTrans;//В Unity SampleScene следи Main камерата
+    }
+}
