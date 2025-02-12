e3d76f48f1890d19028e847949ef6138540ca20a
Bat_Gosho <goshkodark25@gmail.com>
2025-02-12 19:53:19 +0200

    Update FirstFile.cs

--- FirstFile.cs
+++ FirstFile.cs
@@ -9,6 +9,7 @@ using static UnityEngine.GraphicsBuffer;
 public class FirstFile : MonoBehaviour
 {
 
+
     [SerializeField] private float speed = 8f;//скорост
     [SerializeField] private float rotationSpeed = 70;//скорост на въртене
     [SerializeField] private float gravity = -26f;//сила на гравитацията(отрилцателна, за да дърпа към земята)
